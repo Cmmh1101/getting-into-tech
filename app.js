352530feea9c695a2e598f8f608ca02ec0aba5e6
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-02-22 01:23:28 -0600

    added path section styles and scroll effects

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// skewed hero triangles ===================
 const TriangleOne = document.querySelector('.triangleOne');
 const TriangleTwo = document.querySelector('.triangleTwo');
 
@@ -7,3 +8,14 @@ window.addEventListener('scroll', function() {
     TriangleOne.style.transform = "skewY(" + value1 + "deg)";
     TriangleTwo.style.transform = "skewY(" + value2 + "deg)";
 })
+
+// end of skewed hero triangles ===================
+
+// ============== SCROLL REVEAL ===================
+
+ScrollReveal().reveal('#hero');
+ScrollReveal().reveal('#intro', { delay: 500 });
+ScrollReveal().reveal('#path', { delay: 500 });
+// ScrollReveal().reveal('#contactme', { delay: 500 });
+
+// ============== END OF SCROLL REVEAL ===================
