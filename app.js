d833f1149c629937cbc28ec876e79374b2e817b9
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-02-23 14:31:48 -0600

    removed unused images, added scroll down hide nav effect, aded favicon,

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+    } else {
+        document.getElementById("nav").style.top = "-70px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
 // skewed hero triangles ===================
 const TriangleOne = document.querySelector('.triangleOne');
 const TriangleTwo = document.querySelector('.triangleTwo');
